3297b24953d67b1298b030bf82992f977d2b40a0
Dushyanth Reddy <aareakhila@gmail.com>
2026-06-09 23:57:50 +0530

    uPDATED

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,8 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: 'https://talent-flow-beige-pi.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
