ec195eb1ec6b15edb83da5dfb719f1c856bab246
Dushyanth Reddy <aareakhila@gmail.com>
2026-06-09 23:37:18 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,8 @@ const jobRoutes = require('./routes/jobRoutes');
 const adminRoutes = require("./routes/adminRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
 const profileRoutes = require("./routes/profileRoutes");
-const aiRoutes = require("./routes/aiRoutes");
 const path = require('path');
-dotenv.config({ path: path.resolve(__dirname, '.env'), override: true });
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 // Connect MongoDB
 connectDB();
@@ -18,8 +17,14 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://talent-flow-beige-pi.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
@@ -28,7 +33,6 @@ app.use('/api/jobs', jobRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/applications", applicationRoutes);
 app.use("/api/profile", profileRoutes);
-app.use("/api/ai", aiRoutes);
 app.get('/', (req, res) => {
   res.json({ message: 'API is running...' });
 });
